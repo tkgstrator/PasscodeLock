926a57ba8a46b3aa1c55cc906ed226265bbaa484
tkgstrator <nasawake.am@gmail.com>
2021-03-31 20:12:08 +0900

    Initial Commit

---
+++ PasscodeLock/PasscodeLock.h
@@ -0,0 +1,18 @@
+//
+//  PasscodeLock.h
+//  PasscodeLock
+//
+//  Created by Devonly on 3/31/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasscodeLock.
+FOUNDATION_EXPORT double PasscodeLockVersionNumber;
+
+//! Project version string for PasscodeLock.
+FOUNDATION_EXPORT const unsigned char PasscodeLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasscodeLock/PublicHeader.h>
+
+
